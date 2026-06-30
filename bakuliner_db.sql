44a2fb2b9b7db0a62cc44d081b587ee2ef922d13
Maulida rahmah <kimyurinza@gmail.com>
2026-06-30 17:30:24 +0800

    Add files via upload

---
+++ bakuliner_db.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Jun 2026 pada 03.20
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bakuliner_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+-- Kesalahan membaca struktur untuk tabel bakuliner_db.kategori: #1932 - Table 'bakuliner_db.kategori' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel bakuliner_db.kategori: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakuliner_db`.`kategori`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kuliner`
+--
+-- Kesalahan membaca struktur untuk tabel bakuliner_db.kuliner: #1932 - Table 'bakuliner_db.kuliner' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel bakuliner_db.kuliner: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakuliner_db`.`kuliner`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menu`
+--
+-- Kesalahan membaca struktur untuk tabel bakuliner_db.menu: #1932 - Table 'bakuliner_db.menu' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel bakuliner_db.menu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakuliner_db`.`menu`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+-- Kesalahan membaca struktur untuk tabel bakuliner_db.pesanan: #1932 - Table 'bakuliner_db.pesanan' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel bakuliner_db.pesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakuliner_db`.`pesanan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ulasan`
+--
+-- Kesalahan membaca struktur untuk tabel bakuliner_db.ulasan: #1932 - Table 'bakuliner_db.ulasan' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel bakuliner_db.ulasan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakuliner_db`.`ulasan`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
